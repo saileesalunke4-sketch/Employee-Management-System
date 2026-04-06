2fcf897be24a2a9527469ef5c7de3e16e895f2e0
Sailee Salunke <saileesalunke4@gmail.com>
2026-04-06 10:07:40 +0530

    Fixed db password to root

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = " ";
+$host     = "localhost";
+$user     = "root";
+$password = "root";
 $database = "emp1_db";
 
 $conn = mysqli_connect($host, $user, $password, $database);
+
+// If empty password fails try "root"
+if(!$conn){
+    $conn = mysqli_connect($host, $user, "root", $database);
+}
 
 if(!$conn){
     die("Connection Failed: " . mysqli_connect_error());
