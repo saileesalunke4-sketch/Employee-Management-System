de4df9cea0deef190253930d3545e4bee1a390b8
Sailee Salunke <saileesalunke4@gmail.com>
2026-05-20 15:53:53 +0530

    updation done in session time out

--- db.php
+++ db.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('session.gc_maxlifetime', 1800);
+ini_set('session.cookie_lifetime', 1800);
+session_set_cookie_params(1800);
 $host     = "localhost";
 $user     = "root";
 $password = "root";
